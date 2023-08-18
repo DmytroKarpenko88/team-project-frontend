186fd249316f64816789745c0229e83329074ccb
Dmytro Karpenko <kardmitriy@gmail.com>
2023-08-19 00:08:52 +0300

    add bootstrap library

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import 'modern-normalize';
 import { Global, ThemeProvider } from '@emotion/react';
 import { App } from 'components/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter
-    //  basename="/team-project-frontend"
-    >
+    <BrowserRouter basename="/team-project-frontend">
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyles} />
         <App />
