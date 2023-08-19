4971d8ecf232970b8302fffea13e1f97c1c292ea
DmytroKarpenko88 <88401783+DmytroKarpenko88@users.noreply.github.com>
2023-08-19 19:52:39 +0000

    Deploying to gh-pages from @ DmytroKarpenko88/team-project-frontend@bc1ac7e98d7f85303a2ef828735a14246a9a0446 🚀

--- static/js/664.54a27e57.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkteam_project_frontend=self.webpackChunkteam_project_frontend||[]).push([[664],{664:function(e,n,t){function u(){}t.r(n),t.d(n,{default:function(){return u}})}}]);
-//# sourceMappingURL=664.54a27e57.chunk.js.map
