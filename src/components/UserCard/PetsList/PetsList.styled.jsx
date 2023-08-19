347030dbfa0f8a6f035132fae1fc0e0cabaebc43
Sergii Podobrii <podobri@ukr.net>
2023-08-19 11:03:45 +0300

    Added PetsBtn PetsData PetsList

---
+++ src/components/UserCard/PetsList/PetsList.styled.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const PetsListStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+
+  @media screen and (min-width: 768px) {
+    align-items: flex-start;
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 20px;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: 0.8px;
+  color: #000000;
+  @media screen and (min-width: 768px) {
+    font-size: 28px;
+  }
+`;
