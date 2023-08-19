bf1509e8be595804978245d36ed85a56b4d4c3e3
Dmytro Karpenko <kardmitriy@gmail.com>
2023-08-19 22:45:23 +0300

    Icons

---
+++ src/components/icons/Filer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { SVGStyledStroke } from './SVG.styled';
+
+export const Filer = () => {
+  return (
+    <SVGStyledStroke
+      xmlns="http://www.w3.org/2000/svg"
+      width="25"
+      height="24"
+      viewBox="0 0 25 24"
+      fill="none"
+    >
+      <path
+        d="M4.75781 4L9.75781 12V18L15.7578 21V12L20.7578 4H4.75781Z"
+        stroke=""
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </SVGStyledStroke>
+  );
+};
