01dfaf66c226fab6defd362786de0e5785d12bd8
MaiiaHa <maiia.robeiko@ukr.net>
2023-08-21 15:38:41 +0300

    Update AddPetForm.jsx

--- src/components/AddPetForm/AddPetForm.jsx
+++ src/components/AddPetForm/AddPetForm.jsx
@@ -1,6 +1,6 @@
+import { ArrowLeft, Paw } from 'components/icons';
 import React from 'react'; // { useState }
 import { Link } from 'react-router-dom';
-import { Button, TextField } from '@mui/material';
 
 // import StepTitles from './stepTitles';
 
@@ -11,12 +11,14 @@ function AddPetForm() {
   const insertForm = () => {};
   // const title = getTitle(data);
 
-  // if () {
+  // if (click on btn next) {
   //   return setStep(2);
   // }
 
   return (
     <form>
+      {/* --------------------- title of form ---------------------- */}
+
       <div>
         <h2 className="title">
           Add pet
@@ -36,124 +38,17 @@ function AddPetForm() {
         <li>More info</li>
       </ul>
 
-      {/* ----------- first step content */}
+      {/* ----------- content for form based on step -------------- */}
+      <getFormBasedOnStep
+      // step={step} data={data}
+      />
 
-      <form>
-        <label>
-          <input type="radio" name="option" value="red" checked />
-          your pet
-        </label>
-        <label>
-          <input type="radio" name="option" value="green" />
-          sell
-        </label>
-        <label>
-          <input type="radio" name="option" value="green" />
-          lost/found
-        </label>
-        <label>
-          <input type="radio" name="option" value="green" />
-          in good hands
-        </label>
-      </form>
-
-      {/* ----------- second step content */}
-
-      <form>
-        {/* тут умова для Title of add поле в разі певних опшинів радіокнопок*/}
-        <label>
-          Title of add
-          <TextField
-            // <input> tag
-            // id={nameRandomId}
-            autoFocus
-            className="inputField"
-            type="text"
-            placeholder="Title of add"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label className="input">
-          Pet's name
-          <TextField
-            // <input> tag
-            // id={nameRandomId}
-            autoFocus
-            className="inputField"
-            type="text"
-            placeholder="Type name pet"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label className="input">
-          Date of birth
-          <TextField
-            // <input> tag
-            // id={nameRandomId}
-            autoFocus
-            className="inputField"
-            type="text"
-            placeholder="Type date of birth"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label className="input">
-          Type
-          <TextField
-            // <input> tag
-            // id={nameRandomId}
-            autoFocus
-            className="inputField"
-            type="text"
-            placeholder="Type of pet"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-
-        <Button
-          className="inputBtn"
-          variant="contained"
-          type="submit"
-          aria-label="Add contact"
-        >
-          Add contact
-        </Button>
-      </form>
-
-      {/* ----------- third step content */}
-
-      <div className="thirdStep">
-        <label id="photo">
-          Add photo
-          <input type="file" name="photo" alt="pet`s photo" required />
-          <img alt="pet preview"></img>
-        </label>
-
-        <label id="comments">
-          Comments
-          <textarea
-            type="text"
-            name="comments"
-            placeholder="Field is required"
-            required
-          />
-        </label>
-      </div>
-
-      {/* ----------- */}
-
+      {/* ------------------------ buttons ------------------------ */}
       <ul>
         <li>
           <button type="button" onClick={() => insertForm()}>
-            Next
+            Next or Done
+            <Paw />
             {/* <svg class="icon" width="32" height="32">
               <use href="../../icons/paw1.svg"></use>
             </svg> */}
@@ -162,7 +57,9 @@ function AddPetForm() {
 
         <li>
           {/* повернути на сторінку з якої прийшов */}
-          <Link to="/user">Cancel or Back</Link>
+          <Link to="/user">
+            <ArrowLeft /> Cancel or Back
+          </Link>
         </li>
       </ul>
     </form>
