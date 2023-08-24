e37dccab27e7d6f345b5f036eabf1cebb6d3f83c
Dmytro Karpenko <kardmitriy@gmail.com>
2023-08-24 13:22:57 +0300

    logout

--- src/components/Logout/LogoutBtn.jsx
+++ src/components/Logout/LogoutBtn.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react';
 import { Logout } from '../icons/index';
 import { LogoutBtnStyled } from './Logout.styled';
+import { useDispatch } from 'react-redux';
+import { logOut } from 'redux/auth/auth-operations';
 
 const LogoutBtn = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const dispatch = useDispatch();
 
   console.log(isModalOpen);
   const closeModal = () => {
     setIsModalOpen(prevState => !prevState);
+    dispatch(logOut());
   };
 
   return (
     <div style={{ marginTop: '25px' }}>
-      
       <LogoutBtnStyled type="button" onClick={closeModal}>
-       
         <Logout />
         Log Out
       </LogoutBtnStyled>
