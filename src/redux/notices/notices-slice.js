87f9e0052fc42ed486220ec2db6774c3f4535fbe
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2023-08-24 23:41:17 +0200

    added notices slice

---
+++ src/redux/notices/notices-slice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchNotices, addNotice } from './notices-operations';
+
+const noticesInitialState = {
+  items: [],
+  category: 'in good hands',
+  searchQuery: '',
+  isLoading: false,
+  error: null,
+};
+
+const noticesSlice = createSlice({
+    name: 'notices',
+    initialState: noticesInitialState,
+  extraReducers: builder =>
+    builder
+    .addCase(fetchNotices.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addCase(addNotice.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+        state.isLoading = false;
+        state.error = null;
+      })
+})
+
+export const noticesReducer = noticesSlice.reducer;
