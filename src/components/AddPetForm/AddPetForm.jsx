50147ccdf54786bfa80bbe0de0b0267781bc5a06
MaiiaHa <maiia.robeiko@ukr.net>
2023-08-26 19:10:44 +0300

    validation pet

--- src/components/AddPetForm/AddPetForm.jsx
+++ src/components/AddPetForm/AddPetForm.jsx
@@ -1,6 +1,6 @@
 import { ArrowLeft, Paw } from 'components/icons';
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import StepTitles from './StepTitles/StepTitles';
 import getFormBasedOnStep from './getFormBasedOnStep';
 import getTitle from './getTitle';
@@ -14,11 +14,27 @@ import {
   AddPetBtnCancel,
   AddPetBtnCancelDiv,
 } from './AddPetForm.styled';
+import { Formik } from 'formik';
+import { validatePetSchema } from './validatePet';
+import { addNotice } from 'redux/notices/notices-operations';
+import { useDispatch } from 'react-redux';
 
 const AddPetForm = () => {
-  // const location = useLocation();
+  const location = useLocation();
+  const dispatch = useDispatch();
   const [step, setStep] = useState(1);
-  const [data, setData] = useState({ option: 'pet' });
+  const [data, setData] = useState({
+    option: 'pet',
+    name: '',
+    title: '',
+    birthday: '',
+    breed: '',
+    location: '',
+    comments: '',
+    petPhoto: null,
+    sex: '',
+    price: 0,
+  });
 
   const title = getTitle(data);
 
@@ -39,8 +55,52 @@ const AddPetForm = () => {
     }
   };
 
+  const handleSubmit = async () => {
+    if (!data.option) return;
+
+    const newFormData = new FormData();
+
+    newFormData.append('name', data.name);
+    newFormData.append('birthday', data.birthday);
+    newFormData.append('breed', data.breed);
+    newFormData.append('pets-photo', data.petPhoto);
+
+    if (data.comments) {
+      newFormData.append('comments', data.comments);
+    }
+
+    if (data.option === 'pet') {
+      // dispatch(addMyPet(newFormData));
+      // toggleModal();
+      return;
+    }
+
+    newFormData.append('titleOfAdd', data.title);
+    newFormData.append('sex', data.sex);
+    newFormData.append('location', data.location);
+
+    if (data.option === 'lostFound') {
+      dispatch(addNotice({ option: 'lostFound', newFormData }));
+      // toggleModal();
+      return;
+    }
+
+    if (data.option === 'hands') {
+      dispatch(addNotice({ option: 'in-good-hands', newFormData }));
+      // toggleModal();
+      return;
+    }
+
+    newFormData.append('price', data.price);
+
+    if (data.option === 'sell') {
+      dispatch(addNotice({ option: data.option, newFormData }));
+      // toggleModal();
+    }
+  };
+
   // console.log(location); //{pathname: '/add-pet', search: '', hash: '', state: null, key: 'default'}
-  const backPage = step === 1 ? '/user' : '';
+  const backPage = step === 1 ? location.state?.from ?? '/user' : '';
 
   return (
     <AddPetDiv data={data} step={step}>
@@ -48,7 +108,14 @@ const AddPetForm = () => {
         <AddPetFormTitle>{title}</AddPetFormTitle>
         <StepTitles step={step} />
 
-        {getFormBasedOnStep(step, data, setData)}
+        <Formik
+          initialValues={data}
+          validationSchema={validatePetSchema}
+          onSubmit={handleSubmit}
+          validateOnChange={false}
+        >
+          {getFormBasedOnStep(step, data, setData)}
+        </Formik>
 
         <AddPetBtnList>
           <AddPetBtnItem>
