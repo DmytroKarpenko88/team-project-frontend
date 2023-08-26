702cfafb7324bb59558be73b0008ef87a0b29891
Dmytro Karpenko <kardmitriy@gmail.com>
2023-08-26 19:07:28 +0300

    404

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { Button, ContainerPg, H1Text, PicContainer, Image } from './PageNotFound.styled';
+import {
+  Button,
+  ContainerPg,
+  H1Text,
+  PicContainer,
+  Image,
+} from './PageNotFound.styled';
 
 import { Paw } from 'components/icons';
 const PageNotFound = () => {
   return (
     <>
       <ContainerPg>
-        <H1Text>Oops! <br /> This page not found...</H1Text>
+        <H1Text>
+          Oops! <br /> This page not found...
+        </H1Text>
         <PicContainer>
-           <Image alt="not found"/>
+          <Image alt="not found" />
         </PicContainer>
-        <Button
-        type="button"
-        ><span>To main page</span><span><Paw/>
-        </span>
+        <Button to={'/'}>
+          <span>To main page</span>
+          <span>
+            <Paw />
+          </span>
         </Button>
       </ContainerPg>
     </>
