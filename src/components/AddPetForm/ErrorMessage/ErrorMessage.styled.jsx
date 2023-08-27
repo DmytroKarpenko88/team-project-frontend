53274301d70a190da7013a5b6742d49f51390a2d
MaiiaHa <maiia.robeiko@ukr.net>
2023-08-28 00:57:51 +0300

    upd

--- src/components/AddPetForm/ErrorMessage/ErrorMessage.styled.jsx
+++ src/components/AddPetForm/ErrorMessage/ErrorMessage.styled.jsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
+import { theme } from 'styles';
 
 export const ErrorText = styled.p`
   position: absolute;
   bottom: -4px;
   left: 16px;
 
-  font-size: ${({ theme }) => theme.fontSizes[0]};
+  font-size: ${theme.fontSizes.l};
   line-height: 1.33;
-  color: ${({ theme }) => theme.colors.red};
+  color: ${theme.colors.red};
   transform: translateY(100%);
 `;
