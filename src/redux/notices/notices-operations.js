3b615a8942c66e6706a46eee1fa47049dc34709e
Dmytro Karpenko <kardmitriy@gmail.com>
2023-08-27 19:18:32 +0300

    notice

--- src/redux/notices/notices-operations.js
+++ src/redux/notices/notices-operations.js
@@ -4,20 +4,65 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://team-project-backend-881k.onrender.com';
 
-export const fetchNotices = createAsyncThunk('notices/fetchAll', async (_, thunkAPI) => {
+export const fetchNotices = createAsyncThunk(
+  'notices/fetchAll',
+  async (_, thunkAPI) => {
     try {
       const response = await axios.get(`/api/notices/`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
-  });
+  }
+);
 
-  export const addNotice = createAsyncThunk('notices/addNotice', async (fields, thunkAPI) => {
+export const addNotice = createAsyncThunk(
+  'notices/addNotice',
+  async (fields, thunkAPI) => {
     try {
       const response = await axios.post('/api/notices', fields);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
-  });
+  }
+);
+
+export const removeNotice = createAsyncThunk(
+  'notices/removeNotice',
+  async (_id, { rejectWithValue }) => {
+    try {
+      await axios.delete(`/api/notices/notice/${_id}`);
+
+      return { _id };
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addFavoriteNotice = createAsyncThunk(
+  'notices/addFavoriteNotice',
+  async (pet, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post(`/api/notices/favorite/${pet._id}`);
+
+      return data.result.updatedNotice;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const removeFavoriteNotice = createAsyncThunk(
+  'notices/removeFavoriteNotice',
+  async (pet, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.delete(`/api/notices/favorite/${pet._id}`);
+
+      return data.result.updatedNotice;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
