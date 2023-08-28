46681cbe62fbc5fdd2aed503e6a64f0e9fa765b7
Dmytro Karpenko <kardmitriy@gmail.com>
2023-08-28 18:11:12 +0300

    fixed

--- src/components/Modals/NoticeModal/NoticeModal.jsx
+++ src/components/Modals/NoticeModal/NoticeModal.jsx
@@ -26,88 +26,84 @@ import { selectNoticeById } from 'redux/notices/notices-selectors';
 // import { useEffect } from 'react';
 
 export const NoticeModal = props => {
-  const {
-    birthday,
-    describe,
-    location,
-    name,
-    sex,
-    title,
-    type,
-    _owner: { email, phone },
-  } = useSelector(selectNoticeById);
-
-  const cutTitle = title => {
-    return title.length >= 35 ? title.slice(0, 35) + ' ...' : title;
-  };
+  const noticeById = useSelector(selectNoticeById);
+  const { birthday, describe, location, name, sex, title, type, _owner } =
+    noticeById;
+  console.log('noticeById:', noticeById);
 
   return (
-    <ModalContainer {...props} size="lg" centered={true}>
-      <Modal.Body>
-        <CrossBtn onClick={props.onHide}>
-          <Cross />
-        </CrossBtn>
-        <FlexWrapper>
-          <ImgBox>
-            <CategoryName>In good hands</CategoryName>
-            <Img src={img} alt="" />
-          </ImgBox>
-          <div>
-            <Title>{title}</Title>
+    <>
+      {noticeById && (
+        <ModalContainer {...props} size="lg" centered={true}>
+          <Modal.Body>
+            <CrossBtn onClick={props.onHide}>
+              <Cross />
+            </CrossBtn>
+            <FlexWrapper>
+              <ImgBox>
+                <CategoryName>In good hands</CategoryName>
+                <Img src={img} alt="" />
+              </ImgBox>
+              <div>
+                <Title>{title}</Title>
 
-            <Container>
-              <Info>
-                <InfoName>Name:</InfoName>
-                <InfoValues>{name}</InfoValues>
-              </Info>
-              <Info>
-                <InfoName>Birthday: </InfoName>
-                <InfoValues>{birthday}</InfoValues>
-              </Info>
-              <Info>
-                <InfoName>Type:</InfoName>
-                <InfoValues>{type}</InfoValues>
-              </Info>
-              <Info>
-                <InfoName>Place:</InfoName>
-                <InfoValues>{location}</InfoValues>
-              </Info>
-              <Info>
-                <InfoName>The sex:</InfoName>
-                <InfoValues>{sex}</InfoValues>
-              </Info>
-              <Info>
-                <InfoName>Email:</InfoName>
-                <Contact href="mailto:user@mail.com">{email}</Contact>
-              </Info>
-              <Info>
-                <InfoName>Phone:</InfoName>
-                <Contact href="tel:+380971234567">{phone}</Contact>
-              </Info>
-            </Container>
-          </div>
-        </FlexWrapper>
+                <Container>
+                  <Info>
+                    <InfoName>Name:</InfoName>
+                    <InfoValues>{name}</InfoValues>
+                  </Info>
+                  <Info>
+                    <InfoName>Birthday: </InfoName>
+                    <InfoValues>{birthday}</InfoValues>
+                  </Info>
+                  <Info>
+                    <InfoName>Type:</InfoName>
+                    <InfoValues>{type}</InfoValues>
+                  </Info>
+                  <Info>
+                    <InfoName>Place:</InfoName>
+                    <InfoValues>{location}</InfoValues>
+                  </Info>
+                  <Info>
+                    <InfoName>The sex:</InfoName>
+                    <InfoValues>{sex}</InfoValues>
+                  </Info>
+                  <Info>
+                    <InfoName>Email:</InfoName>
+                    <Contact href="mailto:user@mail.com">
+                      {/* {_owner.email} */}
+                    </Contact>
+                  </Info>
+                  <Info>
+                    <InfoName>Phone:</InfoName>
+                    <Contact href="tel:+380971234567">{/* {phone} */}</Contact>
+                  </Info>
+                </Container>
+              </div>
+            </FlexWrapper>
 
-        <InfoMessage>
-          <span style={{ fontFamily: `${theme.fonts.main.semiBold}` }}>
-            Comments:
-          </span>
-          {describe}
-        </InfoMessage>
+            <InfoMessage>
+              <span style={{ fontFamily: `${theme.fonts.main.semiBold}` }}>
+                Comments:
+              </span>
+              {describe}
+            </InfoMessage>
 
-        <BtnWrapper>
-          <BtnContact onClick={props.onHide}>
-            <span>Contact</span>
-          </BtnContact>
+            <BtnWrapper>
+              <BtnContact onClick={props.onHide}>
+                <span>Contact</span>
+              </BtnContact>
 
-          <BtnAddTo onClick={props.onHide}>
-            <span>Add to</span>
-            <span>
-              <Heart />
-            </span>
-          </BtnAddTo>
-        </BtnWrapper>
-      </Modal.Body>
-    </ModalContainer>
+              <BtnAddTo onClick={props.onHide}>
+                <span>Add to</span>
+                <span>
+                  <Heart />
+                </span>
+              </BtnAddTo>
+            </BtnWrapper>
+          </Modal.Body>
+        </ModalContainer>
+      )}
+    </>
   );
 };
