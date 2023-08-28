a9ba02b4d2a7d6c3141da62531a2a536fd09dcbb
Halina Tulchii <halendeihalyna@gmail.com>
2023-08-28 23:33:17 +0300

    Update header
    added some changes to distance between Nav and AuthNav
    
    added closing burger menu functional to AuthNav buttons

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -3,12 +3,12 @@ import { Auth, LinkRegister, LinkLogin, Text } from './AuthNav.styled';
 // import PropTypes from 'prop-types';
 
 
-export const AuthNav = () => {
+export const AuthNav = (onClick) => {
   return (
    
         <Auth>
-          <LinkLogin to="/login" ><Text>Log IN </Text><Paw/></LinkLogin>
-          <LinkRegister to="/register"><Text>Registration</Text></LinkRegister>
+          <LinkLogin to="/login" onClick={onClick}><Text>Log IN </Text><Paw/></LinkLogin>
+          <LinkRegister to="/register" onClick={onClick}><Text>Registration</Text></LinkRegister>
         </Auth>
       )}
     
