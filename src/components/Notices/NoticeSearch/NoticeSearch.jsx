f15835b5f95d37f5626f78d8872343d58affd173
Dmytro Karpenko <kardmitriy@gmail.com>
2023-09-01 17:23:22 +0300

    update

--- src/components/Notices/NoticeSearch/NoticeSearch.jsx
+++ src/components/Notices/NoticeSearch/NoticeSearch.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Search, Cross } from 'components/icons';
